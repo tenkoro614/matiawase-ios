26473e0237969f9c0a43c6019a6de01d2ce59d18
HirokiNakamura <hiroknak@nakamura-no-MacBook-Air.local>
2014-02-27 23:52:59 +0900

    Initial Commit

---
+++ mytestapp/com_tenkoro614AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_tenkoro614AppDelegate.h
+//  mytestapp
+//
+//  Created by HirokiNakamura on 2014/02/27.
+//  Copyright (c) 2014年 HirokiNakamura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_tenkoro614AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mytestapp/com_tenkoro614ViewController.m
@@ -0,0 +1,29 @@
+//
+//  com_tenkoro614ViewController.m
+//  mytestapp
+//
+//  Created by HirokiNakamura on 2014/02/27.
+//  Copyright (c) 2014年 HirokiNakamura. All rights reserved.
+//
+
+#import "com_tenkoro614ViewController.h"
+
+@interface com_tenkoro614ViewController ()
+
+@end
+
+@implementation com_tenkoro614ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
